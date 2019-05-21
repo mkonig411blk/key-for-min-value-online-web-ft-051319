74de557c22836d1524f268d54cd708a5c5017111
Margot Konig <margot.konig@blackrock.com>
2019-05-21 16:45:30 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -4,10 +4,13 @@
 {chair: 30, table: 50, desk: 15}
 
 def key_for_min_value(hash)
+    lowest_value = 30
+    lowest_key = chair
     hash.each do |key, value|
-    lowest_value = 0
-    lowest_key = nil
-     if lowest_value == 0 || value < lowest_value
+     if lowest_value == nil
+      lowest_value = value 
+      lowest_key = key
+    elsif value < lowest_value
       lowest_value = value 
       lowest_key = key
     end
